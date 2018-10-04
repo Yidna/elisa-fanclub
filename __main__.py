56cddc1cb26c48cf21eeecfd275561a361075462
Andy Siu <bge.yidna@gmail.com>
2018-10-04 12:09:46 -0700

    Adjusted main file

--- __main__.py
+++ __main__.py
@@ -1,5 +1,8 @@
 
 
 def main():
-    print("Hello world!")
-    print("Yeah I have no idea how to start a python project")
+    print('Hello world!')
+    print('Yeah I have no idea how to start a python project')
+
+if __name__ == '__main__':
+    main()
