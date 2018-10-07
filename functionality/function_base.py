c7a452f13992981ea460e227be2c73fbeebe2e67
Andy Siu <bge.yidna@gmail.com>
2018-10-07 14:58:19 -0700

    Refactored function_base
    - no longer an abstract base class
    * param_def is now a dict<tuple, callable>
    * check_params now iterates through to match tuple lengths
    + check_params now tries to find a matching <tuple, callable> pair given
      the params
    + check_params now raises an exception using the least offensive params
    - removed run function
    + added run class variable that is set when check_params is called

--- functionality/function_base.py
+++ functionality/function_base.py
@@ -1,9 +1,7 @@
-from abc import ABC as ABSTRACT_CLASS
-from abc import abstractmethod
-from functionality.exceptions.invalid_parameter_length_exception import InvalidParameterLengthException
+from functionality.exceptions import *
 
 
-class FunctionBase(ABSTRACT_CLASS):
+class FunctionBase:
 
     def __init__(self, symbol_table, parameters):
         """
@@ -14,6 +12,7 @@ class FunctionBase(ABSTRACT_CLASS):
         self._symbol_table = symbol_table
         self._parameters = parameters
         self._param_def = self._get_param_def()
+        self._run = None
 
     def execute(self):
         """
@@ -21,7 +20,7 @@ class FunctionBase(ABSTRACT_CLASS):
         :return: anything
         """
         self._check_parameters()
-        return self._run()
+        return self._run(*self._parameters)
 
     def _get_param_def(self):
         """
@@ -29,7 +28,7 @@ class FunctionBase(ABSTRACT_CLASS):
         :return: dict
         """
         return {
-            0: ()
+            (): None
         }
 
     def _check_parameters(self):
@@ -40,17 +39,29 @@ class FunctionBase(ABSTRACT_CLASS):
         """
         # Check if the number of parameters is correct
         param_len = len(self._parameters)
-        if not(param_len in self._param_def):
+        param_options = tuple(p for p in self._param_def if len(p) == param_len)
+        if len(param_options) == 0:
             raise InvalidParameterLengthException(self._param_def, param_len)
 
-        # Check if the types of parameters are correct
-        for i, p in enumerate(self._parameters):
-            self._parameters[i] = self._param_def[param_len][i](self._symbol_table, p).check()
+        parse_dict = {}
+        for option in param_options:
+            parse_dict[option] = []
+            for i, p in enumerate(self._parameters):
+                try:
+                    parse_dict[option].append(option[i](self._symbol_table, p).check())
+                except InvalidParameterException as e:
+                    parse_dict[option].append(e)
 
-    @abstractmethod
-    def _run(self):
-        """
-        The implementation of the function
-        :return: anything
-        """
-        pass
+        least_offended = None
+        least_offended_count = None
+        for option, params in parse_dict.items():
+            invalid_count = sum(p for p in params if isinstance(p, InvalidParameterException))
+            if invalid_count == 0:
+                self._parameters = params
+                self._run = self._param_def[option]
+                return
+            if least_offended is None or invalid_count < least_offended_count:
+                least_offended_count = invalid_count
+                least_offended = option
+
+        raise InvalidParameterException(self._parameters, least_offended)
