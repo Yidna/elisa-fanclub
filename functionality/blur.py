461ce3cae1f5c7a2fa90d35b2bef6cc17f37a19a
Andy Siu <bge.yidna@gmail.com>
2018-10-07 01:35:33 -0700

    Blur function parameter generalization

--- functionality/blur.py
+++ functionality/blur.py
@@ -7,7 +7,7 @@ class Blur(FunctionBase):
 
     def __init__(self, symbol_table, parameters):
         super(Blur, self).__init__(symbol_table, parameters)
-        self._param_length = (2,)
+        self._param_length = (3,)
 
     def _check_parameters(self):
         super(Blur, self)._check_parameters()
@@ -20,12 +20,16 @@ class Blur(FunctionBase):
             raise InvalidParameterException(0, "Variable")
 
         # Check if parameters for the blur are valid
-        param = self._parameters[1]
-        if isinstance(param, tuple) and len(param) == 2 and all(isinstance(value, int) for value in param):
-            self.__filter = self._parameters[1]
+        if isinstance(self._parameters[1], int):
+            self.__x = self._parameters[1]
         else:
-            raise InvalidParameterException(1, "2-Integer Tuple")
+            raise InvalidParameterException(1, "Integer")
+
+        if isinstance(self._parameters[2], int):
+            self.__y = self._parameters[2]
+        else:
+            raise InvalidParameterException(2, "Integer")
 
     def _run(self):
-        self.__image = cv2.GaussianBlur(self.__image, self.__filter, 0)
+        self.__image = cv2.GaussianBlur(self.__image, (self.__x, self.__y), 0)
         self._symbol_table[self.__image_name] = self.__image
