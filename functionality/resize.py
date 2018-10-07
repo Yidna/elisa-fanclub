516175b4c1d9bf912bb9e4529181edb0dcc7354a
Andy Siu <bge.yidna@gmail.com>
2018-10-07 00:34:36 -0700

    Refactored resize function better argument generality

--- functionality/resize.py
+++ functionality/resize.py
@@ -7,7 +7,7 @@ class Resize(FunctionBase):
 
     def __init__(self, symbol_table, parameters):
         super(Resize, self).__init__(symbol_table, parameters)
-        self._param_length = (3,)
+        self._param_length = (2, 3)
 
     def _check_parameters(self):
         super(Resize, self)._check_parameters()
@@ -19,10 +19,10 @@ class Resize(FunctionBase):
         else:
             raise InvalidParameterException(0, "Variable")
 
-        # Check if the resize value is a tuple of 1 or 2 integers
-        param = self._parameters[1]
-        if isinstance(param, tuple) and 0 < param.length <= 2 and all(isinstance(value, int) for value in param):
-            self.__x_scale, self.__y_scale = param if param.length == 2 else (param[0],) * 2
+        # Check if the resize value consists of integers
+        if all(isinstance(value, int) for value in self._parameters[1:]):
+            self.__x_scale = self._parameters[1]
+            self.__y_scale = self._parameters[2] if len(self._parameters) == 2 else self._parameters[1]
         else:
             raise InvalidParameterException(1, "[1, 2]-Integer Tuple")
 
