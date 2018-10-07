f9d6d771e3d655b8255e6b70fde8e4f1256d6f0d
Ryan Lee <ryanmkitlee@gmail.com>
2018-10-07 12:23:19 -0700

    implemented darken

--- functionality/darken.py
+++ functionality/darken.py
@@ -1,6 +1,9 @@
 from functionality.function_base import FunctionBase
 from functionality.typedef import *
 
+import cv2
+import numpy as np
+
 
 class Darken(FunctionBase):
 
@@ -10,5 +13,27 @@ class Darken(FunctionBase):
         }
 
     def _run(self):
-        # TODO
-        pass
+        """
+        When we execute darken, we want to be able to adjust the gamma by 0 <= darken <= 100
+        If user provides upper or lower bound we will adjust to the min and max
+        :return:
+        """
+        img_name, darken = self._parameters[0], self._parameters[1]
+        if darken < 0:
+            darken = 0
+        if darken > 100:
+            darken = 100
+        img = self._symbol_table[img_name]
+        img = self.adjust_gamma(img, gamma=np.abs(darken/100 - 1))
+        self._symbol_table[img_name] = img
+        return img
+
+    def adjust_gamma(self, img, gamma=1.0):
+        # build a lookup table mapping the pixel values [0, 255] to
+        # their adjusted gamma values
+        inv_gamma = 1.0 / gamma
+        table = np.array([((i / 255.0) ** inv_gamma) * 255
+                          for i in np.arange(0, 256)]).astype("uint8")
+
+        # apply gamma correction using the lookup table
+        return cv2.LUT(img, table)
