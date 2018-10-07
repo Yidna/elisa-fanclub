d1b68fa99ec0b50c629f61ee8887dcc5e9d7e3bd
Andy Siu <bge.yidna@gmail.com>
2018-10-07 01:33:40 -0700

    Refactored function to use functionality module
    * used different imports for functionalitiy module
    + started setting up for tokenizer extension

--- ast/function.py
+++ ast/function.py
@@ -1,11 +1,23 @@
 from libs import node
 from libs import symbol_table as st
+from functionality import *
 from scipy import signal
 import cv2
 import numpy as np
 
 
 class FUNCTION(node.Node):
+    FUNC_MAP = {
+        'blur': Blur,
+        'draw': Draw,
+        'darken': Darken,
+        'brighten': Brighten,
+        'grayscale': Grayscale,
+        'resize': Resize,
+        # TODO: implement find
+        'crop': Crop
+        # TODO: implement tile
+    }
     func_name = None
     using_variable = None
     parameters = []
@@ -23,94 +35,29 @@ class FUNCTION(node.Node):
             # self.parameters.extend([int(param_1[:-1]), int(tokenizer.get_next())])
             self.parameters = (int(param_1[:-1]), int(tokenizer.get_next()))
         else:
-            self.parameters = int(param_1)
+            self.parameters = (int(param_1),)
 
         tokenizer.get_and_check_next('to')
         self.to_variable = tokenizer.get_next()
 
     def evaluate(self):
-        img = None
-
+        func_type = None
         if self.func_name == 'blur':
-            if not self.using_variable:
-                img = st.symbol_table[str(self.to_variable)]
-                # TODO: Use parameter to adjust blur not just set to a 13x13 gaussian filter
-                img = cv2.GaussianBlur(img, (13, 13), 0)
-                st.symbol_table[str(self.to_variable)] = img
-            else:
-                raise NotImplementedError('Can you really blur with something onto something?')
-
+            func_type = Blur
         if self.func_name == 'draw':
-            if type(self.parameters) is int:
-                raise AssertionError('Draw must have two arguments')
-
-            x, y = self.parameters
-
-            using_img = st.symbol_table[self.using_variable]
-            to_img = st.symbol_table[self.to_variable]
-            if using_img.shape > to_img.shape:
-                img = using_img
-            else:
-                x_offset, y_offset, _ = using_img.shape
-                to_img[x:x+x_offset, y:y+y_offset] = using_img
-                img = to_img
-
-            st.symbol_table[self.to_variable] = img
-
+            func_type = Draw
         if self.func_name == 'darken':
-            if type(self.parameters) is not int:
-                raise AssertionError('You can only darken with one value')
-            img = st.symbol_table[self.to_variable]
-            # TODO
-            st.symbol_table[self.to_variable] = img
-
+            func_type = Darken
         if self.func_name == 'brighten':
-            img = st.symbol_table[self.to_variable]
-            img = self.increase_brightness(img, self.parameters)
-            st.symbol_table[self.to_variable] = img
-
-        if self.func_name == 'gray':
-            img = st.symbol_table[self.to_variable]
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            st.symbol_table[self.to_variable] = img
-
+            func_type = Brighten
+        if self.func_name == 'grayscale':
+            func_type = Grayscale
         if self.func_name == 'resize':
-            img = st.symbol_table[self.to_variable]
-            if type(self.parameters) is int:
-                x, y = img.shape[:2]
-                x, y = x * self.parameters // 100, y * self.parameters // 100
-            else:
-                x, y = self.parameters
-
-            img = cv2.resize(img, (x, y), interpolation=cv2.INTER_CUBIC)
-            st.symbol_table[self.to_variable] = img
-
-        if self.func_name == 'find':
-            pass
-
+            func_type = Resize
         if self.func_name == 'crop':
-            img = st.symbol_table[self.to_variable]
-            if type(self.parameters) is int:
-                raise Exception('Cannot crop with only one argument')
-            x, y = self.parameters
-            width, height = img.shape[:2]
-            x0, x1, y0, y1 = width // 2 - x // 2, width // 2 + x // 2, height // 2 - y // 2, height // 2 + y // 2
-            img = img[x0:x1, y0:y1]
-            st.symbol_table[self.to_variable] = img
+            func_type = Crop
 
-        if self.func_name == 'tile':
-            pass
-
-        return img
-
-    def increase_brightness(self, img, value):
-        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        h, s, v = cv2.split(hsv)
-
-        lim = 255 - value
-        v[v > lim] = 255
-        v[v <= lim] += value
-
-        final_hsv = cv2.merge((h, s, v))
-        img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
-        return img
+        raw_params = [self.using_variable, self.to_variable, *self.parameters]
+        params = [p for p in raw_params if p is not None]
+        func = func_type(st.symbol_table, params)
+        return func.execute()
--- functionality/__init__.py
+++ functionality/__init__.py
@@ -1,1 +1,7 @@
-__all__ = ["blur", "draw", "darken", "brighten", "grayscale", "resize", "crop"]
+from functionality.blur import Blur
+from functionality.draw import Draw
+from functionality.darken import Darken
+from functionality.brighten import Brighten
+from functionality.grayscale import Grayscale
+from functionality.resize import Resize
+from functionality.crop import Crop
