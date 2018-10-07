8ac259bdd6fb795003b09ff5d8bdd926d42c084e
Andy Siu <bge.yidna@gmail.com>
2018-10-07 02:12:44 -0700

    Function refactored to take advantage of new tokenizer functionality

--- ast/function.py
+++ ast/function.py
@@ -24,7 +24,7 @@ class FUNCTION(node.Node):
     to_variable = None
 
     def parse(self, tokenizer):
-        self.func_name = tokenizer.get_next()
+        self.func_name = tokenizer.get_and_check_next(self.FUNC_MAP)
 
         if tokenizer.get_and_check_next('using'):
             self.using_variable = tokenizer.get_next()
@@ -41,22 +41,7 @@ class FUNCTION(node.Node):
         self.to_variable = tokenizer.get_next()
 
     def evaluate(self):
-        func_type = None
-        if self.func_name == 'blur':
-            func_type = Blur
-        if self.func_name == 'draw':
-            func_type = Draw
-        if self.func_name == 'darken':
-            func_type = Darken
-        if self.func_name == 'brighten':
-            func_type = Brighten
-        if self.func_name == 'grayscale':
-            func_type = Grayscale
-        if self.func_name == 'resize':
-            func_type = Resize
-        if self.func_name == 'crop':
-            func_type = Crop
-
+        func_type = self.FUNC_MAP[self.func_name]
         raw_params = [self.using_variable, self.to_variable, *self.parameters]
         params = [p for p in raw_params if p is not None]
         func = func_type(st.symbol_table, params)
